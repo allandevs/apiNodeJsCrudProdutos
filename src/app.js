3579761f2568e5c66d7deb831e681ec97487465c
Allan Oliveira <allan_rj5@hotmail.com>
2020-10-26 22:41:13 -0300

    Liberação acesso Api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
  const app = express();
 const router = express.Router();
@@ -24,6 +25,14 @@ const indexRoute = require('./routes/index-route')
 const productRoute = require('./routes/product-route')
 const customerRoute = require('./routes/customer-route')
 
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+  }
+
+  app.use(cors(corsOptions))
+
+  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
@@ -31,5 +40,13 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
-
+// app.use(cors()) //Essa linha aqui
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 module.exports = app;
